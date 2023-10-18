c2dab75ac1e022c381bf648eca9e44b44c7e856d
BerlianAnggun <145645074+BerlianAnggun@users.noreply.github.com>
2023-10-18 11:15:21 +0700

    Delete dart_application_anggun directory

--- dart_application_anggun/test/dart_application_anggun_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_application_anggun/dart_application_anggun.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
